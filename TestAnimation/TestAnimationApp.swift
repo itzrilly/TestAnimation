b611e6546e50ba412b9a72c458654e0b4b87d882
itzrilly <32282998+itzrilly@users.noreply.github.com>
2022-08-08 21:32:23 +0100

    Initial Commit

---
+++ TestAnimation/TestAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAnimationApp.swift
+//  TestAnimation
+//
+//  Created by RILLY on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
