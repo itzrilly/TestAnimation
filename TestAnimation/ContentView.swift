ed55cdd95735324289510a27d7168bdc007b0f11
itzrilly <32282998+itzrilly@users.noreply.github.com>
2022-08-09 09:02:02 +0100

    Animer l'apparition et la disparition d'un élément: Afficher ou cacher des vues en les animant

--- TestAnimation/ContentView.swift
+++ TestAnimation/ContentView.swift
@@ -9,28 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var dragAmount = CGSize.zero
-    private let gradient = Gradient(colors: [.purple, .blue])
-
+    @State private var isShowing = false
+    
     var body: some View {
-        LinearGradient(gradient: gradient, startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .cornerRadius(20)
-            .offset(dragAmount)
-            //.animation(.spring(), value: dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged({ value in
-                        withAnimation(.spring()){
-                            dragAmount = value.translation
-                        }
-                    })
-                    .onEnded({ _ in
-                        withAnimation(.easeOut(duration: 3)){
-                            dragAmount = .zero
-                        }
-                    })
-            )
+        VStack(spacing: 20){
+            if(isShowing){
+                Text("Hello")
+            }
+            Button {
+                isShowing.toggle()
+            } label: {
+                Text(isShowing ? "Hide" : "Show")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(isShowing ? Color.orange : Color.blue)
+                    .cornerRadius(10)
+            }
+
+        }
     }
 }
 
